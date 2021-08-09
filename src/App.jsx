0ba83bcf9a7f2ac521af5e3b6f48cfa665412cf5
André <andreglock@gmail.com>
2021-08-09 15:20:43 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,28 @@ import { useState } from 'react';
 import './App.css';
 import Login from './components/Login';
 import TodoList from './components/TodoList';
+import { UserContext } from './contexts/UserContext';
 
 function App() {
   const [user, setUser] = useState(null);
 
-  return (
-    <div className="App">
-      <h1>Codelicious TODO</h1>
+  // Provide default value: 
+  const userValue = { user, setUser };
 
-      {user && <>
-        <p>Welcome, {user.name} <a href="#" onClick={() => { setUser(null) }}>Log out</a></p>
-        <hr/>
-        <TodoList user={user} />
-      </>}
-      {!user && <Login setUser={setUser} />}
-    </div>
+  return (
+    // Add context:
+    <UserContext.Provider value={userValue}>
+      <div className="App">
+        <h1>Codelicious TODO</h1>
+
+        {user && <>
+          <p>Welcome, {user.name} <a href="#" onClick={() => { setUser(null) }}>Log out</a></p>
+          <hr/>
+          <TodoList user={user} />
+        </>}
+        {!user && <Login setUser={setUser} />}
+      </div>
+    </UserContext.Provider>
   );
 }
 
---
+++ src/contexts/UserContext.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const defaultValue = null;
+
+export const UserContext = React.createContext(defaultValue);
